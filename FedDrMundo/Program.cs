23edeea8f786ab9f0b00a66366d06b9e076630dd
gFederal <forkelitept@ig.com.br>
2014-09-12 12:46:21 -0300

    update

--- FedDrMundo/Program.cs
+++ FedDrMundo/Program.cs
@@ -26,14 +26,19 @@ namespace FedDrMundo
         public static Spell E;
         public static Spell R;
 
+        private static SpellSlot IgniteSlot;
+        private static SpellSlot SmiteSlot;
+        
         public static bool WActive = false;
 
-        public static Menu Config;
+        public static Menu Config;        
+        public static Menu TargetedItems;
+        public static Menu NoTargetedItems;
 
         private static Obj_AI_Hero Player;
 
         private static void Main(string[] args)
-        {
+        {            
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
 
@@ -48,6 +53,9 @@ namespace FedDrMundo
             E = new Spell(SpellSlot.E, Player.AttackRange + 25);
             R = new Spell(SpellSlot.R, Player.AttackRange + 25);
 
+            IgniteSlot = Player.GetSpellSlot("SummonerDot");
+            SmiteSlot = Player.GetSpellSlot("SummonerSmite");
+            
             Q.SetSkillshot(0.50f, 75f, 1500f, true, SkillshotType.SkillshotLine);            
 
             SpellList.AddRange(new[] { Q, W, E, R });
@@ -64,7 +72,8 @@ namespace FedDrMundo
             Config.AddSubMenu(new Menu("Combo", "Combo"));
             Config.SubMenu("Combo").AddItem(new MenuItem("UseQCombo", "Use Q").SetValue(true));
             Config.SubMenu("Combo").AddItem(new MenuItem("UseWCombo", "Use W").SetValue(true));
-            Config.SubMenu("Combo").AddItem(new MenuItem("UseECombo", "Use E").SetValue(true));            
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseECombo", "Use E").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseItensCombo", "Use Items in Combo").SetValue(true));
             Config.SubMenu("Combo").AddItem(new MenuItem("ComboActive", "Combo!").SetValue(new KeyBind(32, KeyBindType.Press)));            
 
             Config.AddSubMenu(new Menu("Harass", "Harass"));            
@@ -83,10 +92,13 @@ namespace FedDrMundo
             Config.SubMenu("JungleFarm").AddItem(new MenuItem("UseQJFarm", "Use Q").SetValue(true));
             Config.SubMenu("JungleFarm").AddItem(new MenuItem("UseWJFarm", "Use W").SetValue(true));
             Config.SubMenu("JungleFarm").AddItem(new MenuItem("UseEJFarm", "Use E").SetValue(true));            
+            Config.SubMenu("JungleFarm").AddItem(new MenuItem("AutoSmite", "Auto Smite!").SetValue<KeyBind>(new KeyBind('J', KeyBindType.Toggle)));
             Config.SubMenu("JungleFarm").AddItem(new MenuItem("JungleFarmActive", "JungleFarm!").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
 
             Config.AddSubMenu(new Menu("Misc", "Misc"));
+            Config.SubMenu("Misc").AddItem(new MenuItem("laugh", "Troll laugh?").SetValue(true));
             Config.SubMenu("Misc").AddItem(new MenuItem("KS", "Killsteal using Q").SetValue(false));
+            Config.SubMenu("Misc").AddItem(new MenuItem("AutoI", "Auto Ignite").SetValue(true));
             Config.SubMenu("Misc").AddItem(new MenuItem("RangeQ", "Q Range Slider").SetValue(new Slider(980, 1000, 0)));
             Config.SubMenu("Misc").AddItem(new MenuItem("lifesave", "Life saving Ultimate").SetValue(true));
             Config.SubMenu("Misc").AddItem(new MenuItem("percenthp", "Life Saving Ult %").SetValue(new Slider(30, 100, 0)));
@@ -102,6 +114,27 @@ namespace FedDrMundo
             Config.AddSubMenu(new Menu("Drawings", "Drawings"));
             Config.SubMenu("Drawings").AddItem(new MenuItem("QRange", "Q range").SetValue(new Circle(true, Color.FromArgb(255, 255, 255, 255))));
             Config.SubMenu("Drawings").AddItem(dmgAfterComboItem);
+
+            Config.AddSubMenu(new Menu("Itens", "Itens"));
+            Menu menuUseItems = new Menu("Use Items", "menuUseItems");
+            Config.SubMenu("Itens").AddSubMenu(menuUseItems);
+
+            TargetedItems = new Menu("Targeted Items", "menuTargetItems");
+            menuUseItems.AddSubMenu(TargetedItems);
+            TargetedItems.AddItem(new MenuItem("item3153", "Blade of the Ruined King").SetValue(true));
+            TargetedItems.AddItem(new MenuItem("item3143", "Randuin's Omen").SetValue(true));
+            TargetedItems.AddItem(new MenuItem("item3144", "Bilgewater Cutlass").SetValue(true));
+            TargetedItems.AddItem(new MenuItem("item3146", "Hextech Gunblade").SetValue(true));
+            TargetedItems.AddItem(new MenuItem("item3184", "Entropy ").SetValue(true));
+            
+            NoTargetedItems = new Menu("AOE Items", "menuNonTargetedItems");
+            menuUseItems.AddSubMenu(NoTargetedItems);
+            NoTargetedItems.AddItem(new MenuItem("item3180", "Odyn's Veil").SetValue(true));
+            NoTargetedItems.AddItem(new MenuItem("item3131", "Sword of the Divine").SetValue(true));
+            NoTargetedItems.AddItem(new MenuItem("item3074", "Ravenous Hydra").SetValue(true));
+            NoTargetedItems.AddItem(new MenuItem("item3077", "Tiamat ").SetValue(true));
+            NoTargetedItems.AddItem(new MenuItem("item3142", "Youmuu's Ghostblade").SetValue(true)); 
+
             Config.AddToMainMenu();
 
             Game.OnGameUpdate += Game_OnGameUpdate;
@@ -119,7 +152,7 @@ namespace FedDrMundo
                 if (menuItem.Active)
                 {
                     Utility.DrawCircle(Player.Position, spell.Range, menuItem.Color);
-                }
+                }                
             }
         }
 
@@ -177,8 +210,29 @@ namespace FedDrMundo
 
             if (Config.Item("KS").GetValue<bool>())
                 Killsteal();
+
+            if (Config.Item("AutoSmite").GetValue<KeyBind>().Active)            
+                AutoSmite();
+
+            if (Config.Item("AutoI").GetValue<bool>())            
+                AutoIgnite();             
         }
 
+        private static void AutoIgnite()
+        {
+            var iTarget = SimpleTs.GetTarget(600, SimpleTs.DamageType.True);
+            var Idamage = DamageLib.getDmg(iTarget, DamageLib.SpellType.IGNITE) * 0.80;
+
+            if (IgniteSlot != SpellSlot.Unknown && Player.SummonerSpellbook.CanUseSpell(IgniteSlot) == SpellState.Ready && iTarget.Health < Idamage)
+            {
+                Player.SummonerSpellbook.CastSpell(IgniteSlot, iTarget);
+                if (Config.Item("laugh").GetValue<bool>())
+                {
+                    Game.Say("/l");
+                }
+            }
+        }
+        
         private static void LifeSave()
         {
             int inimigos = Utility.CountEnemysInRange(900);
@@ -202,13 +256,82 @@ namespace FedDrMundo
                     Q.Cast(qPred.CastPosition);
             }
         }
+
+        private static InventorySlot GetInventorySlot(int ID)
+        {
+            return ObjectManager.Player.InventoryItems.FirstOrDefault(item => (item.Id == (ItemId)ID && item.Stacks >= 1) || (item.Id == (ItemId)ID && item.Charges >= 1));
+        }
+
+        public static void UseItems(Obj_AI_Hero vTarget)
+        {
+            if (vTarget != null)
+            {
+                foreach (MenuItem menuItem in TargetedItems.Items)
+                {
+                    var useItem = TargetedItems.Item(menuItem.Name).GetValue<bool>();
+                    if (useItem)
+                    {
+                        var itemID = Convert.ToInt16(menuItem.Name.ToString().Substring(4, 4));
+                        if (Items.HasItem(itemID) && Items.CanUseItem(itemID) && GetInventorySlot(itemID) != null)
+                            Items.UseItem(itemID, vTarget);
+                    }
+                }
+
+                foreach (MenuItem menuItem in NoTargetedItems.Items)
+                {
+                    var useItem = NoTargetedItems.Item(menuItem.Name).GetValue<bool>();
+                    if (useItem)
+                    {
+                        var itemID = Convert.ToInt16(menuItem.Name.ToString().Substring(4, 4));
+                        if (Items.HasItem(itemID) && Items.CanUseItem(itemID) && GetInventorySlot(itemID) != null)
+                            Items.UseItem(itemID);
+                    }
+                }
+            }
+        }
+
+        private static void AutoSmite()
+        {
+            if (Config.Item("AutoSmite").GetValue<KeyBind>().Active)
+            {
+                float[] SmiteDmg = { 20 * Player.Level + 370, 30 * Player.Level + 330, 40 * Player.Level + 240, 50 * Player.Level + 100 };
+                string[] MonsterNames = { "LizardElder", "AncientGolem", "Worm", "Dragon" };                
+                var vMinions = MinionManager.GetMinions(Player.ServerPosition, Player.SummonerSpellbook.Spells.FirstOrDefault(
+                    spell => spell.Name.Contains("smite")).SData.CastRange[0], MinionTypes.All, MinionTeam.NotAlly, MinionOrderTypes.Health);
+                foreach (var vMinion in vMinions)
+                {
+                    if (vMinion != null
+                        && !vMinion.IsDead
+                        && !Player.IsDead
+                        && !Player.IsStunned
+                        && SmiteSlot != SpellSlot.Unknown
+                        && Player.SummonerSpellbook.CanUseSpell(SmiteSlot) == SpellState.Ready)
+                    {
+                        if ((vMinion.Health < SmiteDmg.Max()) && (MonsterNames.Any(name => vMinion.BaseSkinName.StartsWith(name))))
+                        { 
+                            Player.SummonerSpellbook.CastSpell(SmiteSlot, vMinion);
+
+                            if (Config.Item("laugh").GetValue<bool>())
+                            {
+                                Game.Say("/l");
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
         
         private static void Combo()
         {
             int qRange = Config.Item("RangeQ").GetValue<Slider>().Value;
 
-            var qTarget = SimpleTs.GetTarget(Q.Range + Q.Width, SimpleTs.DamageType.Magical);           
+            var qTarget = SimpleTs.GetTarget(Q.Range + Q.Width, SimpleTs.DamageType.Magical);
 
+            if (qTarget != null && Config.Item("UseItensCombo").GetValue<bool>())
+            {
+                UseItems(qTarget);
+            }
             if (qTarget != null && Config.Item("UseQCombo").GetValue<bool>() && Q.IsReady() && Player.Distance(qTarget) <= qRange)
             {
                     PredictionOutput qPred = Q.GetPrediction(qTarget);
@@ -220,7 +343,7 @@ namespace FedDrMundo
                 W.Cast();
             }
             if (qTarget != null && Config.Item("UseECombo").GetValue<bool>() && E.IsReady() && Player.Distance(qTarget) <= qRange)
-            {
+            {                
                 E.Cast();                
             }            
         }        
